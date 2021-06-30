e5af4e26efa351ab20c8a0b903beefd66039f0f8
Sergiy Gogolenko <sergiy.gogolenko@gmail.com>
2021-06-30 21:49:04 +0200

    [ansible] Fixed configuration issues and improved build script for Urika-CS at HLRS
    - enabled choice of modules to benchmark
    - fixed issue imposed by security settings for Spark
    - replace Cloudera repo with Apache where necessary
    - fixed regex in Ansible script

--- HiBench/ansible/scripts/vulcan/build_hibench.sh
+++ HiBench/ansible/scripts/vulcan/build_hibench.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
+############################################################
+## @file
+## @copyright (C) 2021 HLRS
+##    All rights reserved.
+##
+## Use, modification, and distribution is subject to the license.
+##
+## @author Sergiy Gogolenko <gogolenko@hlrs.de>
+##
+############################################################
 # submit job with: qsub -l select=1:node_type=clx-21:UNS=True,walltime=00:30:00 
 
-module load bigdata/spark_cluster
-init-spark
+[[ -z "$SPARK_HOME" ]] && module load bigdata/spark_cluster && init-spark
 
 export HIBENCH_HOME={{ hibench_prefix }}
-
 export PATH=$PATH:{{ maven_prefix }}/bin
 
 HADOOP_VERSION=$(hadoop version | grep -e ^Hadoop\\s*\.*\$ | sed -E "s/(^Hadoop\s+)([\.0-9]+).*/\2/")
 SPARK_VERSION=$(spark-submit --version 2>&1 | grep -e ^\.*\\s*version\\s*\.*\$ | grep -v Scala | sed -E "s/(^.+version\s+)([\.0-9]+).*/\2/")
 SCALA_VERSION=$(spark-submit --version 2>&1 | grep -e ^\.*\\s*version\\s*\.*\$ | grep Scala | sed -E "s/(^.+version\s+)([\.0-9]+).*/\2/")
 
-cd $HIBENCH_HOME && mvn -Dspark=$SPARK_VERSION -Dscala=$SCALA_VERSION -Dspark.version=$SPARK_VERSION clean package
-# ./bin/prepare_all.sh
+# # Enable proxying with port forwarding to have access to Internet
+# # @TODO: use ~/.m2/settings.xml for this purpose
+# http_proxy=http://127.0.0.1:8118
+# https_proxy=http://127.0.0.1:8118
+# HTTP_PROXY=http://127.0.0.1:8118
+# HTTPS_PROXY=http://127.0.0.1:8118
+
+# Compile benchmarks
+# @NOTE:
+#   - For some reason, setting up Scala version does not work: "-Dscala=$SCALA_VERSION"
+#   - Pwebsearch requires Apache Mahout
+cd $HIBENCH_HOME && \
+    mvn {{ ['-P'] | product(hibench_frameworks | product(['bench']) | map('join') | list) | map('join') | list | join(' ') }} \
+	-Dmodules {{ ['-P'] | product(hibench_modules) | map('join') | list | join(' ') }} \
+	-Dspark=$SPARK_VERSION -Dspark.version=$SPARK_VERSION clean package
--- HiBench/ansible/scripts/vulcan/init_hibench.sh
+++ HiBench/ansible/scripts/vulcan/init_hibench.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
+############################################################
+## @file
+## @copyright (C) 2021 HLRS
+##    All rights reserved.
+##
+## Use, modification, and distribution is subject to the license.
+##
+## @author Sergiy Gogolenko <gogolenko@hlrs.de>
+##
+############################################################
 # submit job with: qsub -l select=1:node_type=clx-21:UNS=True,walltime=00:30:00 
 
-[[-z "$SPARK_HOME" ]] && module load bigdata/spark_cluster && init-spark
+[[ -z "$SPARK_HOME" ]] && module load bigdata/spark_cluster && init-spark
 
 export HIBENCH_HOME={{ hibench_prefix }}
 export HADOOP_HOME=$HADOOP_PREFIX
 # export SPARK_HOME=$SPARK_HOME
 
-SPARK_CONF_DIR=~/bigdata/$PBS_JOBID/spark-conf
+export SPARK_CONF_DIR=~/bigdata/$PBS_JOBID/spark-conf
 SPARK_MASTER=$(cat $SPARK_CONF_DIR/spark-defaults.conf | grep spark.master | sed -E "s/(^spark.master\s+)(\w+)/\2/") \
     && SPARK_SLAVES=$(cat $SPARK_CONF_DIR/slaves | sed 'N;s/\n/,/') \
     && SPARK_NUM_SLAVES=$(cat $SPARK_CONF_DIR/slaves | wc -l) \
     && echo "master: $SPARK_MASTER. $SPARK_NUM_SLAVES slaves: $SPARK_SLAVES"
 
-sed -i -E "s/(^hibench.masters.hostnames\s+)(.+)/\1${SPARK_MASTER}/" $HIBENCH_HOME/conf/hibench.conf
-sed -i -E "s/(^hibench.slaves.hostnames\s+)(.+)/\1${SPARK_SLAVES}/" $HIBENCH_HOME/conf/hibench.conf
+sed -i -E "s@(^hibench\.masters\.hostnames\s*)(.*)@\1 ${SPARK_MASTER}@" $HIBENCH_HOME/conf/hibench.conf
+sed -i -E "s@(^hibench\.slaves\.hostnames\s*)(.*)@\1 ${SPARK_SLAVES}@" $HIBENCH_HOME/conf/hibench.conf
 
-sed -i -E "s/(^hibench.hadoop.home\s+)(.+)/\1${HADOOP_HOME}/" $HIBENCH_HOME/conf/hadoop.conf
+sed -i -E "s@(^hibench\.hadoop\.home\s*)(.*)@\1 ${HADOOP_HOME}@" $HIBENCH_HOME/conf/hadoop.conf
 
-sed -i -E "s/(^hibench.spark.home\s+)(.+)/\1${SPARK_HOME}/" $HIBENCH_HOME/conf/spark.conf
-sed -i -E "s/(^hibench.yarn.executor.num\s+)(.+)/\1${SPARK_NUM_SLAVES}/" $HIBENCH_HOME/conf/spark.conf
+sed -i -E "s@(^hibench.spark.home\s*)(.*)@\1 ${SPARK_HOME}@" $HIBENCH_HOME/conf/spark.conf
+sed -i -E "s@(^hibench.yarn.executor.num\s*)(.*)@\1 ${SPARK_NUM_SLAVES}@" $HIBENCH_HOME/conf/spark.conf
 # Set up Spark master for standalone mode
 #   for YARN mode use: yarn-client
-sed -i -E "s/(^hibench.spark.master\s+)(.+)/\1${SPARK_MASTER}/" $HIBENCH_HOME/conf/spark.conf
+sed -i -E "s@(^hibench.spark.master\s*)(.*)@\1 ${SPARK_MASTER}@" $HIBENCH_HOME/conf/spark.conf
+# Copy extra Spark configs to ensure security (valid for Vulcan)
+# TODO: make this workaround more generic
+sed    -E "/(^spark\.master\s*)(.*)/d" $SPARK_CONF_DIR/spark-defaults.conf >> $HIBENCH_HOME/conf/spark.conf
 
-SPARK_CHECKPOINTIING_DIR=$(cat $SPARK_CONF_DIR/spark-defaults.conf | grep hibench.streambench.spark.checkpointPath | \
+
+SPARK_CHECKPOINTIING_DIR=$(cat $HIBENCH_HOME/conf/spark.conf | grep hibench.streambench.spark.checkpointPath | \
 			       sed -E "s/(^hibench.streambench.spark.checkpointPath\s+)(.+)/\2/")
 mkdir -p $SPARK_CHECKPOINTIING_DIR
